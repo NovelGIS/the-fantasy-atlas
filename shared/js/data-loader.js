680be74ab7ca3706e0324a7c3474bc3e261e0172
NovelGIS <smport0510@gmail.com>
2026-04-30 14:11:05 -0500

    update data-loader and map-core

--- shared/js/data-loader.js
+++ shared/js/data-loader.js
@@ -2,21 +2,17 @@
 
 function loadAllData() {
     return Promise.all([
-
     fetch(CONFIG.dataFiles.land).then(r => r.json()),
     fetch(CONFIG.dataFiles.regions).then(r => r.json()),
     fetch(CONFIG.dataFiles.habitat).then(r => r.json()),
     fetch(CONFIG.dataFiles.poilines).then(r => r.json()),
     fetch(CONFIG.dataFiles.water).then(r => r.json()),
-    fetch(CONFIG.dataFiles.poi)
-        .then(r => {
-            console.log("POI fetch status:", r.status);
-            return r.json();
-        }),
+    fetch(CONFIG.dataFiles.poi).then(r => {
+        console.log("POI RAW RESPONSE OK");
+        return r.json();
+    }),
     fetch(CONFIG.dataFiles.characters).then(r => r.json())
-        
-
-    ]).then(([land, regions, habitat, poilines, water, poi, characters]) => {
+]).then(([land, regions, habitat, poilines, water, poi, characters]) => {
 
         window.landData = land;
         window.regionsData = regions;
