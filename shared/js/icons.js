0e10d00496499c52f43f0c3dc9f58fe1023055af
NovelGIS <smport0510@gmail.com>
2026-04-29 21:46:17 -0500

    Update icons.js

--- shared/js/icons.js
+++ shared/js/icons.js
@@ -1,12 +1,12 @@
 var starIcon = L.icon({
-    iconUrl: '/shared/assets/icons/star.png',
+    iconUrl: 'shared/assets/icons/star.png',
     iconSize: [25, 25],       // size of the icon
     iconAnchor: [12, 12],     // center point
     popupAnchor: [0, -10]
 });
 
 var towerIcon = L.icon({
-    iconUrl: '/shared/assets/icons/blackrectangle.png',
+    iconUrl: 'shared/assets/icons/blackrectangle.png',
     iconSize: [15, 15],       // size of the icon
     iconAnchor: [12, 12],     // center point
     popupAnchor: [0, -10]
